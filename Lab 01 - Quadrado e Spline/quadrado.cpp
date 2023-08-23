1cbd964b50117fb0765bcec55fc8f72b3277781d
MauricioPimenta <mauriciobpimenta@gmail.com>
2023-08-23 19:14:44 -0300

    Até 2.1 feito - quadrado mexendo com wasd

--- Lab 01 - Quadrado e Spline/quadrado.cpp
+++ Lab 01 - Quadrado e Spline/quadrado.cpp
@@ -1,50 +1,148 @@
+/***********************************************************************
+ * Arquivo: quadrado.cpp
+ *
+ * Autor: Maurício Bittencourt Pimenta
+ *
+ * Descrição:
+ *
+ * Aprendendo a usar OpenGL, GLU e Glut
+ * Desenha um quadrado e realiza algumas operações
+ ***********************************************************************/
+
+
+/*
+ * Bibliotecas usadas no codigo
+ */
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
 #include <stdio.h>
-#define TAMANHO_JANELA 500
+
+
+/*
+ * Macros e Constantes
+ */
+#define TAMANHO_JANELA 500		// Tamanho inicial da janela
+#define WIN_X	10			// Posicao inicial da janela (x,y) em relacao
+#define WIN_Y	10			// ao canto superior esquerdo
+
+
+/*
+ * Variaveis Globais Necessárias
+ */
+
+// Armazenar posicao e tamanho do quadrado
+float qX = 0.25, qY = 0.25;
+float qL = 0.5;
+
+/*
+ * Cabecalhos de Funcoes do Programa
+ */
+void display(void);
+void init (void);
+void Redisplay(void);
+void KeyPress(unsigned char key, int x, int y);
+
+
+/*
+ *	The All Powerfull MAIN
+ */
+
+int main(int argc, char** argv)
+{
+	// Inicializa o glut com argumentos da main
+    glutInit(&argc, argv);
+
+	// Inicializa o modo do display
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+
+	// Define o tamanho e a posicao da janela
+    glutInitWindowSize (TAMANHO_JANELA, TAMANHO_JANELA);
+    glutInitWindowPosition (WIN_X, WIN_Y);
+
+	// Cria a Janela com um Nome
+	int windows = glutCreateWindow ("My Gorgeous Looking Square!");
+
+	// Local function to set OpenGL initial configurations
+	init ();
+
+	// Define callback Functions
+    glutDisplayFunc(display);		// Display Function
+	glutKeyboardFunc(KeyPress);		// Keyboard function
+
+
+	// Inicia o Loop Principal do Programa
+    glutMainLoop();
+
+    return 0;
+}
+
+
+/*-------------------------------------------------------------------------
+ *
+ *						Funções Usadas no Programa
+ *
+ *-------------------------------------------------------------------------*/
 
 void display(void)
 {
-   /* Limpar todos os pixels  */
-   glClear (GL_COLOR_BUFFER_BIT);
+	/* Limpar todos os pixels  */
+	glClear (GL_COLOR_BUFFER_BIT);
 
-   /* Define cor dos vértices com os valores R, G e B variando de 0.0 a 1.0 */
-   glColor3f (1.0, 0.6, 0.2);
-   /* Desenhar um polígono branco (retângulo) */
-   glBegin(GL_POLYGON);
-      glVertex3f (0.25, 0.25, 0.0);
-      glVertex3f (0.75, 0.25, 0.0);
-      glVertex3f (0.75, 0.75, 0.0);
-      glVertex3f (0.25, 0.75, 0.0);
-   glEnd();
+	/* Define cor dos vértices com os valores R, G e B variando de 0.0 a 1.0 */
+	glColor3f (1.0, 0.6, 0.2);
+	/* Desenhar um polígono branco (retângulo) */
+	glBegin(GL_POLYGON);
+		glVertex3f (qX, qY, 0.0);
+		glVertex3f (qX + qL, qY, 0.0);
+		glVertex3f (qX + qL, qY + qL, 0.0);
+		glVertex3f (qX, qY + qL, 0.0);
+	glEnd();
 
-   /* Desenhar no frame buffer! */
-   glutSwapBuffers(); // Funcao apropriada para janela double buffer
+	/* Desenhar no frame buffer! */
+	glutSwapBuffers(); // Funcao apropriada para janela double buffer
 }
 
 void init (void)
 {
-  /* selecionar cor de fundo (preto) */
-  //glClearColor (0.0, 0.0, 0.0, 0.0);
+	/* selecionar cor de fundo (preto) */
+	glClearColor (0.0, 0.0, 0.0, 0.0);
 
-  /* inicializar sistema de visualizacao */
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
+	/* inicializar sistema de visualizacao */
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
-int main(int argc, char** argv)
+void Redisplay(void){
+
+}
+
+void KeyPress(unsigned char key, int x, int y)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB);
-    glutInitWindowSize (TAMANHO_JANELA, TAMANHO_JANELA);
-    glutInitWindowPosition (100, 100);
-    glutCreateWindow ("hello world");
-    init ();
-    glutDisplayFunc(display);
+	// Check key presses and move the square.
+	if (key == 'w')
+	{
+		qY += 0.01;
+	}
+	else if (key == 's')
+	{
+		qY -= 0.01;
+	}
+	else if (key == 'a')
+	{
+		qX -= 0.01;
+	}
+	else if (key == 'd')
+	{
+		qX += 0.01;
+	}
+	else
+	{
+		return;
+	}
 
-    glutMainLoop();
+	// Draw again
+	glutPostRedisplay();
 
-    return 0;
 }
