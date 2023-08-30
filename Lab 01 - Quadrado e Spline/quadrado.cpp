d10db11f0c97177b9b31ca7a5ab516118bfc5caa
Maurício Bittencourt Pimenta <mauriciobpimenta@gmail.com>
2023-08-30 12:15:39 -0300

    Quadrado funcionando!! Arraste e movimento com as teclas do mouse.

--- Lab 01 - Quadrado e Spline/quadrado.cpp
+++ Lab 01 - Quadrado e Spline/quadrado.cpp
@@ -7,6 +7,8 @@
  *
  * Aprendendo a usar OpenGL, GLU e Glut
  * Desenha um quadrado e realiza algumas operações
+ * - Movimenta o quadrado com as teclas 'w,a,s,d' do teclado
+ * - Realiza o drag and drop com o mouse ao clicar dentro do quadrado
  ***********************************************************************/
 
 
@@ -25,31 +27,34 @@
 #define SCREEN_WIDTH	1920
 #define	SCREEN_HEIGHT	1080
 
-#define TAMANHO_JANELA 500		// Tamanho inicial da janela
+#define TAMANHO_JANELA		500		// Tamanho inicial da janela
+
 
 #define WIN_X	SCREEN_WIDTH/2 - TAMANHO_JANELA/2			// Posicao inicial da janela (x,y) em relacao
-#define WIN_Y	SCREEN_HEIGHT/2 - TAMANHO_JANELA/2			// ao canto superior esquerdo
+#define WIN_Y	SCREEN_HEIGHT/2 - TAMANHO_JANELA/2			// ao canto superior esquerdo - Centralizado na tela
 
 
 /*
  * Variaveis Globais Necessárias
  */
 
-// Armazenar posicao e tamanho do quadrado
-float qX = 0.25, qY = 0.25;
-float qL = 0.5;
-
 // Armazena tamanho da janela atual
 int winHeight = TAMANHO_JANELA;
 int winWidth = TAMANHO_JANELA;
+
+// Armazena a posicao do quadrado (x e y)
+float qX = 0.25, qY = 0.25;
+float qLX = 0.5*winWidth/winHeight, qLY = 0.5;	// Armazena o tamanho do quadrado (x e y)
 
 // Variaveis globais e definicoes para funcoes do teclado
 int keyStatus[256];
 
 
 // Variaveis globais para funcoes do mouse
-float mX = 0;	// Armazena posicoes X e Y do mouse
-float mY = 0;
+float mX = 0;		// Armazena posicoes X e Y do mouse
+float mY = 0;		// Armazena posicao Y do mouse
+
+bool dragm = false;		// setado se estiver arrastando um objeto
 
 
 
@@ -74,26 +79,24 @@ void dragMouse(int x, int y);
 
 int main(int argc, char** argv)
 {
-	// Inicializa o glut com argumentos da main
-    glutInit(&argc, argv);
+	printf("\n\nQtd de Argumentos argc: %d", argc);
+	printf("\nArgumentos (argv): %s", *argv);
 
-	// Inicializa o modo do display
-    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-
-	printf("\n\nArgumento argc: %d", argc);
-	printf("\nArgumentos (argv): %s", argv[0]);
-
-	if(argc == 1){
+	if(argc == 1)
+	{
 		winHeight = TAMANHO_JANELA;
 		winWidth = TAMANHO_JANELA;
+		printf("\nIniciando programa com o tamanho padrão da Janela: %d x %d\n", TAMANHO_JANELA, TAMANHO_JANELA);
 	}
 	else if (argc == 2)
 	{
-		printf("\n\nargv = %s\n", *argv);
-		winHeight = atoi(argv[1]); printf("\n\nwinHeight: %d\n", winHeight);
+		winHeight = atoi(argv[1]);
 		winWidth = winHeight;
+
+		printf("\n\nwinHeight: %d\n", winHeight);
 	}
-	else if (argc == 3){
+	else if (argc == 3)
+	{
 
 		winHeight = atoi(argv[1]);
 		winWidth = atoi(argv[2]);
@@ -104,6 +107,17 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
+	// Inicializa as teclas do teclado como GLUT_UP
+	for (int i = 0; i < 256; i++)
+	{
+		keyStatus[i] = GLUT_UP;
+	}
+
+	// Inicializa o glut com argumentos da main
+    glutInit(&argc, argv);
+
+	// Inicializa o modo do display
+    glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 
 	// Define o tamanho e a posicao da janela
     glutInitWindowSize (winHeight, winWidth);
@@ -114,6 +128,7 @@ int main(int argc, char** argv)
 
 	// Local function to set OpenGL initial configurations
 	init ();
+	
 
 	// Define callback Functions
     glutDisplayFunc(display);		// Display Function
@@ -128,8 +143,6 @@ int main(int argc, char** argv)
 	glutMotionFunc(dragMouse);		// Callback quando há movimento do mouse E tecla está pressionada
 
 
-
-	//glut
 
 
 	// Inicia o Loop Principal do Programa
@@ -155,9 +168,9 @@ void display(void)
 	/* Desenhar um polígono branco (retângulo) */
 	glBegin(GL_POLYGON);
 		glVertex3f (qX, qY, 0.0);
-		glVertex3f (qX + qL, qY, 0.0);
-		glVertex3f (qX + qL, qY + qL, 0.0);
-		glVertex3f (qX, qY + qL, 0.0);
+		glVertex3f (qX + qLX, qY, 0.0);
+		glVertex3f (qX + qLX, qY + qLY, 0.0);
+		glVertex3f (qX, qY + qLY, 0.0);
 	glEnd();
 
 	/* Desenhar no frame buffer! */
@@ -186,7 +199,8 @@ void KeyPress(unsigned char key, int x, int y)
 
 }
 
-void keyUp(unsigned char key, int x, int y){
+void keyUp(unsigned char key, int x, int y)
+{
 	// change key to 'not pressed'
 	keyStatus[(int) key] = GLUT_UP;
 
@@ -194,29 +208,35 @@ void keyUp(unsigned char key, int x, int y){
 	glutPostRedisplay();
 }
 
-void idle(void){
+void idle(void)
+{
 
-	// Check key presses and move the square.
-	if (keyStatus[(int)('w')] == GLUT_DOWN)
-		qY += 0.008;
-	if (keyStatus[(int)('s')] == GLUT_DOWN)
-		qY -= 0.008;
-	if (keyStatus[(int)('a')] == GLUT_DOWN)
-		qX -= 0.008;
-	if (keyStatus[(int)('d')] == GLUT_DOWN)
-		qX += 0.008;
+	winWidth = glutGet(GLUT_WINDOW_WIDTH);
+	winHeight = glutGet(GLUT_WINDOW_HEIGHT);
 
-	// if(mouseButtonStatus[GLUT_LEFT_BUTTON] == GLUT_DOWN){
-	// 	qX = mouseX;
-	// 	qY = mouseY;
-	// }
+	if (!dragm)
+	{
+		// Check key presses and move the square.
+		if (keyStatus[(int)('w')] == GLUT_DOWN)
+			qY += 0.008;
+		else if (keyStatus[(int)('s')] == GLUT_DOWN)
+			qY -= 0.008;
+		if (keyStatus[(int)('a')] == GLUT_DOWN)
+			qX -= 0.008;
+		else if (keyStatus[(int)('d')] == GLUT_DOWN)
+			qX += 0.008;
+	}
 
 	// Redraw
 	glutPostRedisplay();
 
 }
 
-void mouse(int button, int state, int x, int y){
+void mouse(int button, int state, int x, int y)
+{
+
+	winWidth = glutGet(GLUT_WINDOW_WIDTH);
+	winHeight = glutGet(GLUT_WINDOW_HEIGHT);
 
 	// Inverte o valor de y, pois por padrao o (0,0) do mouse é no canto superior esquerdo
 	y = winHeight - y;
@@ -225,42 +245,56 @@ void mouse(int button, int state, int x, int y){
 	// button = 0 ou GLUT_LEFT_BUTTON -> botão direito do mouse
 	// state = 0 ou GLUT_DOWN-> botão pressionado
 	if (button == GLUT_LEFT_BUTTON & state == GLUT_DOWN){
-		printf("\n mouse(x,y): (%d,%d)\n", x, y);
 
 		// Armazena coordenadas do mouse em valores de 0 a 1
 		float mouseX = (float) x/winWidth;
 		float mouseY = (float) y/winHeight;
 
-		// Armazena a posicao inicial do clique
-		mX = mouseX;
-		mY = mouseY;
+		printf("\n mouse(x,y): (%d,%d)\tmouseXY: (%.2f,%.2f)\n", x, y, mouseX, mouseY);
+
+		if (mouseX > qX && mouseX < (qX + qLX) && mouseY > qY && mouseY < (qY + qLY))
+		{
+			// Armazena a posicao inicial do clique
+			mX = mouseX;
+			mY = mouseY;
+
+			dragm = true;	// Set Dragging with mouse
+		}
 	}
+	if (state == GLUT_UP)
+		dragm = false;
 
 	glutPostRedisplay();
 
 }
 
-void dragMouse(int x, int y){
-
-	// Inverte o valor de y, pois por padrao o (0,0) do mouse é no canto superior esquerdo
-	y = winHeight - y;
-	printf("\n mouse move (x,y): (%d,%d)\n", x, y);
-
-	// Armazena coordenadas do mouse em valores de 0 a 1
-	float mouseX = (float) x/winWidth;
-	float mouseY = (float) y/winHeight;
+void dragMouse(int x, int y)
+{
 
 	// Se as coordenadas do mouse representarem pontos internos ao quadrado,
 	// permite o arraste do quadrado -> Muda qX e qY
-	if (mouseX > qX && mouseX < (qX + qL) && mouseY > qY && mouseY < (qY + qL))
+	if (dragm)
 	{
+		winWidth = glutGet(GLUT_WINDOW_WIDTH);
+		winHeight = glutGet(GLUT_WINDOW_HEIGHT);
+
+		// Inverte o valor de y, pois por padrao o (0,0) do mouse é no canto superior esquerdo
+		y = winHeight - y;
+
+		//printf("\n mouse move (x,y): (%d,%d)\n", x, y);
+
+		// Armazena coordenadas do mouse em valores de 0 a 1
+		float mouseX = (float) x/winWidth;
+		float mouseY = (float) y/winHeight;
+
 		// Atualiza a posicao do objeto e redesenha.
 		qX += (mouseX - mX);
 		qY += (mouseY - mY);
-	}
 
-	mX = mouseX;
-	mY = mouseY;
+		// Atualiza a posicao atual do mouse
+		mX = mouseX;
+		mY = mouseY;
+	}
 
 
 	glutPostRedisplay();
