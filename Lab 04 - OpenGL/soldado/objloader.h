be79f44a990b07edfc444b74c74b722f3cdec4af
Maurício Bittencourt Pimenta <mauriciobpimenta@gmail.com>
2023-10-31 10:35:46 -0300

    Started the Lab 04 OpenGL 3D - Part 3

---
+++ Lab 04 - OpenGL/soldado/objloader.h
@@ -0,0 +1,49 @@
+#include <GL/gl.h>
+#include <GL/glut.h>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#ifndef OBJ_LOADER_H
+#define OBJ_LOADER_H
+using namespace std;
+
+typedef struct POS{
+    float x;
+    float y;
+    float z;
+} pos;
+
+typedef struct NORM{
+    float x;
+    float y;
+    float z;
+} norm;
+
+typedef struct TEX{
+    float u;
+    float v;
+} tex;
+
+typedef struct VERTICE_STRIP{
+    pos vPos;
+    norm vNorm;
+    tex vTex;
+} verticeStrip;
+
+class mesh{
+
+    public:
+    vector<pos> vertsPos;	// Lista de posicao dos vertices (original do OBJ)
+    vector<norm> vertsNorm; // Lista de normais dos vertices (original do OBJ)
+    vector<tex> vertsTex; 	// Lista de coord de textura dos vertices (original do OBJ)
+
+    vector<verticeStrip> vertsS;	// Lista ordenada de vertices das faces para o desenho do modelo via trangle strip
+
+	// Constructor
+    mesh(){};
+
+	// Public Functions
+    bool loadMesh(string path);//Path é o caminho para o arquivo .obj e deve conter o nome do arquivo.obj
+    void draw();
+};
+#endif
